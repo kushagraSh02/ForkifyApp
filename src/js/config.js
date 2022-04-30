16325c6282a45a449063b13ad996fb4c9bf00e41
kushagraSh02 <kushagra.singh68@gmail.com>
2022-05-01 00:35:05 +0530

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'ae13b053-9e06-4dda-abb4-937e60d3672d';
+export const MODAL_CLOSE_SEC = 2.5;
